b22e4aa48894733d615d39209a8ebb207bfcfa21
Cloud User <ec2-user@ip-10-0-8-191.eu-west-2.compute.internal>
2024-02-17 18:23:35 +0100

    NGINX instead of caddy for prefect and rabbitmq

--- rabbitmq_example/producer.py
+++ rabbitmq_example/producer.py
@@ -4,6 +4,6 @@ channel = connection.channel()
 channel.queue_declare(queue='hello')
 channel.basic_publish(exchange='',
                       routing_key='hello',
-                      body='/mnt/data/cw_data_eng_2/data/wavs/0638.wav') 
+                      body='/mnt/data/cw_data_eng_2/data/0638.wav') 
 print(" [x] Sent 'Hello World!'")
 channel.close()
